e921a85d2600f78e3ded09cab4c1442bee39e78c
cayleb247 <156046197+cayleb247@users.noreply.github.com>
2024-05-07 05:01:37 +0000

    added dynamic data and json endpoint

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+RIDES = [
+    {
+        'id': 1,
+        'name': 'Mike Ross',
+        'location': '4512, Example St.',
+        'ETA': '12 minutes'
+    },
+    {
+        'id': 2,
+        'name': 'Harvey Spector',
+        'location': '3989, Example St.',
+        'ETA': '14 minutes'
+    },
+    {
+        'id': 3,
+        'name': 'Rachel Zane',
+        'location': '4543, Example St.',
+        'ETA': '13 minutes'
+    },
+    {
+        'id': 4,
+        'name': 'Louis Litt',
+        'location': '4259, Example St.'
+    }
+]
+
 @app.route('/') #path of the url after domain name
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', rides=RIDES)
+
+@app.route('/api/rides') #Makes a json endpoint that can be accessed to see RIDES data
+def list_rides():
+    return jsonify(RIDES)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
